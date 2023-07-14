c85acf262f08f5e09326198742cf01332564fcd3
Tan Phat Vo <recca5p@gmail.com>
2023-07-14 13:41:30 +0700

    update gitingore

---
+++ aspnet-core/src/VCBRDemo.Domain/Domains/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace VCBRDemo.Domains
+{
+    public class Customer : AuditedEntity<Guid>
+    {
+    }
+}
